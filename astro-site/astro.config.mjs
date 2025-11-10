ded4231011aa2e50f8f6e9c7b9dd30e64c00feae
august <augustherrstrom@gmail.com>
2025-11-10 21:55:58 +0100

    added daisyui

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   i18n: {
     defaultLocale: 'sv',
     locales: ['sv']
