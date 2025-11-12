1af580529d08c1781b30e9201affea849feefcf2
august <augustherrstrom@gmail.com>
2025-11-12 20:50:11 +0100

    added sitemap

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -4,9 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://kokbok.aghost.io',
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
